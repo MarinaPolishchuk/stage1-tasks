e7fb49169ed0d06b4929b6ffc6d5dcf961363a4d
MarinaPolishchuk <me41.harchenko@mail.com>
2023-02-23 20:31:42 +0200

    feat: add weather and quotes

--- script.js
+++ script.js
@@ -40,7 +40,6 @@ function getTimeOfDay() {
 }
 
 const name = document.querySelector('.name');
-
 function setLocalStorage() {
   localStorage.setItem('name', name.value);
 }
@@ -55,7 +54,6 @@ window.addEventListener('load', getLocalStorage)
 
 const body = document.querySelector('body');
 
-
 function getRandomNum(min = 1, max = 20) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -65,31 +63,31 @@ let randomNum = getRandomNum();
 
 function setBg() {
   const timeOfDay = getTimeOfDay();
-  const bgNum = randomNum +'';
-  const picNum = bgNum.padStart(2,"0");
+  const bgNum = randomNum + '';
+  const picNum = bgNum.padStart(2, "0");
 
   const url = `https://marinapolishchuk.github.io/stage1-tasks/images/${timeOfDay}/${picNum}.jpg`
 
   const img = new Image();
   img.src = url;
-    img.onload = () => {
-      body.style.backgroundImage = `url('${url}')`;
+  img.onload = () => {
+    body.style.backgroundImage = `url('${url}')`;
   };
 }
 setBg();
 
 const nextBt = document.querySelector('.slide-next');
-nextBt.addEventListener ('click', ()=> {
+nextBt.addEventListener('click', () => {
   randomNum++;
-  if(randomNum>20){
-    randomNum =1
+  if (randomNum > 20) {
+    randomNum = 1
   }
   setBg();
 })
 
 const prevBt = document.querySelector('.slide-prev');
 prevBt.addEventListener('click', () => {
-  
+
   if (randomNum > 1) {
     randomNum--;
   } else {
@@ -97,4 +95,70 @@ prevBt.addEventListener('click', () => {
   }
   setBg();
 })
+
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
+city.value = "Minsk";
+const windSpeed = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+
+
+
+async function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=d07b334afb5ad289e48086333ead8f2a&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  temperature.textContent = `${data.main.temp}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+  windSpeed.textContent = `Wind speed: ${data.wind.speed}m/c`;
+  humidity.textContent = `Humidity: ${data.main.humidity}%`;
+}
+getWeather()
+
+function setCity(event) {
+  if (event.code === 'Enter') {
+    getWeather();
+    city.blur();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setCity);
+
+
+function setLocalStorage() {
+  localStorage.setItem('city', city.value);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+  if (localStorage.getItem('city')) {
+    city.value = localStorage.getItem('city');
+  }
+}
+window.addEventListener('load', getLocalStorage)
+
+
+const changeQuoteEl = document.querySelector('.change-quote');
+const quotes = document.querySelector('.quote');
+const author = document.querySelector('.author');
+const quotesNumber = getRandomNum(1, 51);
+let quotesData = [];
+
+async function getQuotes() {
+  const quotesUrl = 'data.json';
+  const res = await fetch(quotesUrl);
+  quotesData = await res.json();
+
+  const q = quotesData[quotesNumber];
+
+  quotes.textContent = q.text;
+  author.textContent = q.author;
+}
+getQuotes();
 
